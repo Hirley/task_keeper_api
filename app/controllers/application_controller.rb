05fe5dc1c0dbbd274a8bbc7bdfc31a7945d9279b
Hirley Esmeraldo Ribeiro <hirley@gmail.com>
2026-08-21 15:06:32 -0300

    Corrige UnknownFormat ao negar acesso em requests .pdf (CanCan)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html { redirect_to root_path, alert: exception.message }
       format.json { render json: { error: exception.message }, status: :forbidden }
+      format.any  { render plain: exception.message, status: :forbidden }
     end
   end
 end
